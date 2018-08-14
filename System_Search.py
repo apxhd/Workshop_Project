9cb1d87f9e87ca7e570cfb398a0c7dac22c36935
Aabiskar Pandey <40898558+apxhd@users.noreply.github.com>
2018-08-15 00:46:57 +0545

    Add files via upload

---
+++ System_Search.py
@@ -0,0 +1,32 @@
+import os
+
+filelist = []
+exten_list = []
+
+extension = input("Which extension do you want to search?")
+final_extension = "." + extension
+for root,dirs,files in os.walk('.',topdown = True):
+    filelist.append(files)
+
+for files in filelist:
+    for file in files:
+        if final_extension in file:
+            exten_list.append(file)
+else:
+    print("Invalid Extension")
+
+
+for exten in exten_list:
+    print(exten)
+
+option = input("Search for a file ?")
+if 'y' in option.lower():
+    filename = input("Enter filename or keyword")
+    for exten in exten_list:
+        if filename.lower() in exten.lower():
+            print(exten)
+    #else:
+#        print("No such filename found!!!")
+else:
+    print("Thanks for using Aabiskar Search Tool")
+           
